4691be275ea3dd781ed197b2cac1b073291e811a
kac049 <kac049@ucsd.edu>
2016-11-21 20:09:28 -0800

    KbCheck added

--- gatherdata.m
+++ gatherdata.m
@@ -13,6 +13,13 @@ function [ DataStruct ] = gatherdata( words, movieArray )
     % full screen
     % [w,wRect]=Screen(screenID,'OpenWindow',0,[],[],2); 
 
+    KbName('UnifyKeyNames'); % set keyboard type
+    %clear all;
+    FlushEvents;
+    while KbCheck;
+    end
+    toWait = 3; % length of time to wait for response
+    
     words = {'arm', 'leg', 'gesf'};
     movieArray = [1, 2, 3];
     for i =1:length(words)
@@ -22,6 +29,35 @@ function [ DataStruct ] = gatherdata( words, movieArray )
         Screen('Flip', w);
         WaitSecs(2);
         %Get some input (KbCheck)
+        %clear all;
+        TrialStart = GetSecs;
+        keyIsDown = 0;
+        FlushEvents;
+        while KbCheck;
+        end
+        
+        while GetSecs < TrialStart + toWait
+            [keyIsDown, secs, keycode] = KbCheck;
+            
+            if keyIsDown
+                DataStruct.response(i) = KbName(keycode);
+                DataStruct.time(i) = secs - TrialStart;
+                keyIsDown = 0;
+                FlushEvents;
+                break
+            else % if no key is pressed  --> still need to discuss
+                DataStruct.response(i) = '';
+                DataStruct.time(i) = ?;
+                FlushEvevnts;
+            end
+            keyIsDown = 0;
+            FlushEvents;
+        end
+        while KbCheck;
+        end
+        
+        if response == no response --> still need to discuss
+        end
     end
     %Do something with input
     
