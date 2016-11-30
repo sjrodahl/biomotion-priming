cc1bd6687d3ff6c7c3449964421a0f670a2c1df9
dmsherwood22 <devmsherwood@gmail.com>
2016-11-29 16:11:40 -0800

    Update on groupData and Analyze

--- analyze.m
+++ analyze.m
@@ -1,14 +1,16 @@
 % Script to analyze conditions given data collected
 
-load trialData.mat
+load trialData.mat  % load matlab file with all subject/trial data
 
-avg_throwArmRT = mean(analysis.throwArmRT);
-avg_throwLegRT = mean(analysis.throwLegRT);
-avg_throwNonRT = mean(analysis.throwNonRT);
-avg_kickArmRT = mean(analysis.kickArmRT);
-avg_kickLegRT = mean(analysis.kickLegRT);
-avg_kickNonRT = mean(analysis.kickNonRT);
+% Calculate average reaction times for each condition (all trials)
+avg_throwArmRT = mean([analysis.throwArmRT]);
+avg_throwLegRT = mean([analysis.throwLegRT]);
+avg_throwNonRT = mean([analysis.throwNonRT]);
+avg_kickArmRT = mean([analysis.kickArmRT]);
+avg_kickLegRT = mean([analysis.kickLegRT]);
+avg_kickNonRT = mean([analysis.kickNonRT]);
 
+% Display average reaction times for each condition
 disp(['Average RT for throw-arm condition is ' ...
     num2str(avg_throwArmRT) ' seconds']);
 disp(['Average RT for throw-leg condition is ' ...
@@ -23,13 +25,15 @@ disp(['Average RT for kick-leg condition is ' ...
 disp(['Average RT for kick non-word condition is ' ...
     num2str(avg_kickNonRT) ' seconds']);
 
+% create array with RT for each condition
 RTarray = [avg_throwArmRT, avg_throwLegRT, ...
     avg_throwNonRT, avg_kickArmRT, avg_kickLegRT, ...
     avg_kickNonRT];
 
-bar(RTarray)
+% create a bar graph illustrating RT for each condition
+bar(RTarray);
 set(gca,'XTickLabel',{'Throw-Arm', 'Throw-Leg', 'Throw-Non', ...
-    'Kick-Arm', 'Kick-Leg', 'Kick-Non'})
+    'Kick-Arm', 'Kick-Leg', 'Kick-Non'});
 
 
 
--- groupData.m
+++ groupData.m
@@ -5,11 +5,13 @@ function groupData(result)
 % Check if the trial data file has already been created 
 fileExist = exist('trialData.mat');
 if fileExist == 2
-    load trialData.mat; % load trial data from other subjects
+    load trialData.mat; % load trial data from other subjects, if exists
+    i = length(analysis) + 1;  % row for individual subject data entry
+else
+    i = 1;
 end
-    
 
-id = result.subjectID;
+analysis(i).id = result.subjectID;
 cond = result.data.conditions;
 numTrials = length(cond);
 
@@ -27,41 +29,37 @@ for n = 1:numTrials
     % throw-arm condition 
     if cond(n) == 1 
         ta = ta + 1;
-        analysis(id).throwArmRT(ta) = result.data.RT(n);
-        analysis(id).throwArmAcc(ta) = result.data.accuracy(n);
+        analysis(i).throwArmRT(ta) = result.data.RT(n);
+        analysis(i).throwArmAcc(ta) = result.data.accuracy(n);
     % throw-leg condition
     elseif cond(n) == 2
         tl = tl + 1;
-        analysis(id).throwLegRT(tl) = result.data.RT(n);
-        analysis(id).throwLegRT(tl) = result.data.accuracy(n);
+        analysis(i).throwLegRT(tl) = result.data.RT(n);
+        analysis(i).throwLegRT(tl) = result.data.accuracy(n);
     % throw non-word condition
     elseif cond(n) == 3
         tn = tn + 1;
-        analysis(id).throwNonRT(tn) = result.data.RT(n);
-        analysis(id).throwNonAcc(tn) = result.data.accuracy(n);
+        analysis(i).throwNonRT(tn) = result.data.RT(n);
+        analysis(i).throwNonAcc(tn) = result.data.accuracy(n);
     % kick-arm condition
     elseif cond(n) == 4
         ka = ka + 1;
-        analysis(id).kickArmRT(ka) = result.data.RT(n);
-        analysis(id).kickArmAcc(ka) = result.data.accuracy(n);
+        analysis(i).kickArmRT(ka) = result.data.RT(n);
+        analysis(i).kickArmAcc(ka) = result.data.accuracy(n);
     % kick-leg condition  
     elseif cond(n) == 5
         kl = kl + 1;
-        analysis(id).kickLegRT(kl) = result.data.RT(n);
-        analysis(id).kickLegAcc(kl) = result.data.accuracy(n);
+        analysis(i).kickLegRT(kl) = result.data.RT(n);
+        analysis(i).kickLegAcc(kl) = result.data.accuracy(n);
     % kick non-word condition  
     elseif cond(n) == 6
         kn = kn + 1;
-        analysis(id).kickNon(kn) = result.data.RT(n);
-        analysis(id).kickNon(kn) = result.data.accuracy(n);
+        analysis(i).kickNonRT(kn) = result.data.RT(n);
+        analysis(i).kickNonAcc(kn) = result.data.accuracy(n);
     end
 end
 
-% Create data arrays for handedness
-if strcmpi(result.hand, 'r')
-    
-
-    
+  
 save trialData.mat analysis
 
 
